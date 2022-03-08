a6e3663679b015852d4acc2c5485957a5088c0d7
Hippolyte EZIAN <polarusdev@gmail.com>
2022-03-08 12:04:46 +0100

    First commit.

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var input, button, ul;
+input = document.querySelector("input");
+button = document.getElementById("enter");
+ul = document.querySelector("ul");
+
+/**
+ *
+ */
+function addLiToUL() {
+  // creer element li
+  if (input.value.trim().length > 0) {
+    li = document.createElement("li");
+    li.textContent = input.value;
+
+    // ajouter li a la liste ul
+    ul.appendChild(li);
+    input.value = "";
+  }
+}
+/**
+ * @param  {String} "click": event name
+ * @param  {Function} addLiToUL : method to add list items
+ */
+button.addEventListener("click", addLiToUL);
+
+/**
+ * @param  {String} "keypress"
+ * @param  {} function(event)
+ */
+input.addEventListener("keypress", (event) => {
+  if (event.code == "Enter") {
+    addLiToUL();
+  }
+});
+
+/**
+ * @param  {Number} a
+ * @param  {Number} b
+ * @retun {Number}
+ */
+function add(a, b) {
+  return a + b;
+}
