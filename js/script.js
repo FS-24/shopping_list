ba0e4cbf7837ae3102c04ab237a28a268b3d8b11
Hippolyte EZIAN <polarusdev@gmail.com>
2022-03-08 16:53:44 +0100

    Update with storing data in local storage.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,27 @@ input = document.querySelector("input");
 button = document.getElementById("enter");
 ul = document.querySelector("ul");
 
-/**
- *
- */
+var shoppingItems = localStorage.getItem("shopping_items")
+  ? JSON.parse(localStorage.getItem("shopping_items"))
+  : [];
+for (const item of shoppingItems) {
+  li = document.createElement("li");
+  li.textContent = item;
+  ul.appendChild(li);
+}
+
+function storeShoppingItemInStorage(value) {
+  shoppingItems.push(value);
+  console.log(typeof shoppingItems);
+  localStorage.setItem("shopping_items", JSON.stringify(shoppingItems));
+}
+
 function addLiToUL() {
   // creer element li
   if (input.value.trim().length > 0) {
     li = document.createElement("li");
     li.textContent = input.value;
-
+    storeShoppingItemInStorage(input.value);
     // ajouter li a la liste ul
     ul.appendChild(li);
     input.value = "";
@@ -32,12 +44,3 @@ input.addEventListener("keypress", (event) => {
     addLiToUL();
   }
 });
-
-/**
- * @param  {Number} a
- * @param  {Number} b
- * @retun {Number}
- */
-function add(a, b) {
-  return a + b;
-}
