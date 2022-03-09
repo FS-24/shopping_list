c99dafab1d2d5dacd5e802ca48c55a965052c515
Hippolyte EZIAN <polarusdev@gmail.com>
2022-03-09 19:47:35 +0100

    first version.

--- js/script.js
+++ js/script.js
@@ -1,46 +1,157 @@
-var input, button, ul;
+var input, button, ul, shoppingItems;
 input = document.querySelector("input");
 button = document.getElementById("enter");
 ul = document.querySelector("ul");
 
-var shoppingItems = localStorage.getItem("shopping_items")
-  ? JSON.parse(localStorage.getItem("shopping_items"))
-  : [];
-for (const item of shoppingItems) {
-  li = document.createElement("li");
-  li.textContent = item;
-  ul.appendChild(li);
+class ShoppingItem {
+  constructor(name, state = false) {
+    this.name = name;
+    this.state = state;
+  }
+}
+/**
+ * @returns shopping items Array from local Storage
+ */
+function getShoppingItemsFromStorage() {
+  return localStorage.getItem("shopping_items")
+    ? JSON.parse(localStorage.getItem("shopping_items"))
+    : [];
 }
 
+/**
+ * load list of shoppingItems and add them to the li
+ */
+function loadExistingElements() {
+  shoppingItems = getShoppingItemsFromStorage();
+  nbreShItem = shoppingItems.length - 1;
+  for (const object of shoppingItems) {
+    if (object.state) {
+      createLiElementAndAppendToUl(object.name).classList.add("done");
+    } else {
+      createLiElementAndAppendToUl(object.name);
+    }
+  }
+}
+
+loadExistingElements();
+
+/**
+ * Create List Element and add it to ul Element
+ *
+ * @param  {String} textContentValue
+ */
+function createLiElementAndAppendToUl(textContentValue) {
+  let li = document.createElement("li");
+  li.textContent = textContentValue;
+  li.classList.add("item");
+  ul.appendChild(li);
+  ul.appendChild(createDeletebutton());
+  return li;
+}
+
+function createDeletebutton() {
+  let btn = document.createElement("button");
+  btn.textContent = "x";
+  return btn;
+}
+/**
+ * Add input value to the Array and
+ * Store shopping list in the localStarage
+ * @param  {String} value => text  of the input
+ */
 function storeShoppingItemInStorage(value) {
-  shoppingItems.push(value);
-  console.log(typeof shoppingItems);
+  shoppingItems.push(new ShoppingItem(value));
+  localStorage.setItem("shopping_items", JSON.stringify(shoppingItems));
+}
+/**
+ * @param  {Number} index => index of the element in the shoppingItems list
+ * @param  {Object} value => the object modified
+ */
+function updateShoppingItemInStorage(index, value) {
+  shoppingItems.splice(index, 1, value);
   localStorage.setItem("shopping_items", JSON.stringify(shoppingItems));
 }
 
-function addLiToUL() {
-  // creer element li
+/**
+ * @param  {Number} index => index of the element in the shoppingItems list
+ * @param  {Object} value => the object modified
+ */
+function deleteShoppingItem(index) {
+  shoppingItems.splice(index, 1);
+  localStorage.setItem("shopping_items", JSON.stringify(shoppingItems));
+}
+/**
+ * check if the input value is empty or not
+ * creat an li and add it to ul and store the data in local storage
+ */
+function addNewItem() {
   if (input.value.trim().length > 0) {
-    li = document.createElement("li");
-    li.textContent = input.value;
+    li = createLiElementAndAppendToUl(input.value);
     storeShoppingItemInStorage(input.value);
-    // ajouter li a la liste ul
-    ul.appendChild(li);
     input.value = "";
+    addEventsToElement(shoppingItems.length, li);
+    console.log(nbreShItem);
   }
 }
 /**
  * @param  {String} "click": event name
- * @param  {Function} addLiToUL : method to add list items
+ * @param  {Function} addNewItem : method to add list items
  */
-button.addEventListener("click", addLiToUL);
+button.addEventListener("click", addNewItem);
 
 /**
  * @param  {String} "keypress"
- * @param  {} function(event)
+ * @param  {Event} function(event)
  */
 input.addEventListener("keypress", (event) => {
   if (event.code == "Enter") {
-    addLiToUL();
+    addNewItem();
   }
 });
+
+/**
+ * Verify if the element classlist contains 'done' class
+ * it remove the class if exist and update the value in the local storage
+ * @param  {String} list Element (li)
+ * @param  {Number} index =>index of Element in shoppingItem list
+ */
+function checkState(index, element) {
+  if (element.classList.contains("done")) {
+    element.classList.remove("done");
+    shoppingItems[index].state = false;
+    updateShoppingItemInStorage(index, shoppingItems[index]);
+  } else {
+    element.classList.add("done");
+    shoppingItems[index].state = true;
+    updateShoppingItemInStorage(index, shoppingItems[index]);
+  }
+}
+
+function addEventsToElement(index, element) {
+  var btns = document.querySelectorAll("ul>button");
+  element.addEventListener("click", () => {
+    checkState(index, element);
+  });
+  btns[index].addEventListener("click", () => {
+    deleteShoppingItem(index);
+    element.remove();
+    btns[index].remove();
+  });
+}
+
+/**
+ * add class and listener to li element and make checkState
+ */
+function addClassToListElements() {
+  let listElements = document.querySelectorAll("ul>li");
+  let btns = document.querySelectorAll("ul>button");
+  var nbreListItem = listElements.length;
+  console.log(btns);
+
+  for (let i = 0; i < nbreListItem; i++) {
+    const element = listElements[i];
+    addEventsToElement(i, element);
+  }
+}
+
+addClassToListElements();
